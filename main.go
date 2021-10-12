b71d21df0792bf2662a1c329061d9ac6821944fe
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-10-12 17:28:27 +0900

    pointers

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@email.com",
+			zipCode: 94000,
+		},
+	}
+
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
